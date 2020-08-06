230378888f045a8427003602d649f0a23abb4fdb
relykstm <relykstm@gmail.com>
2020-08-06 16:52:14 -0700

    thursreact

---
+++ src/modules/Dashboard/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = () => (
+    <div>Dashboard Module</div>
+);
+
+export default {
+    routeProps: {
+        path: '/',
+        exact: true,
+        component: Dashboard,
+    },
+    name: 'Dashboard',
+};
---
+++ src/modules/index.js
@@ -0,0 +1,13 @@
+import Analytics from './Analytics';
+import Bugs from './Bugs'; // added
+import Dashboard from './Dashboard';
+import Games from './Games';
+import Users from './Users';
+
+export default [
+    Dashboard,
+    Games,
+    Bugs, // added
+    Users,
+    Analytics,
+];
